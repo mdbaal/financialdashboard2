0a8f3bbefccf7d647d0ab6133c26a87272137f1c
Mirco Baalmans <mircobaalmans@live.nl>
2025-10-18 22:15:13 +0200

    FIN-71 Added index function

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class CategoryController extends Controller
 {
     //
+    public function index()
+    {
+        $categories = Auth()->user()->categories;
+
+        return Inertia::render('Categories',
+            [
+                'categories' => $categories->load('transactions'),
+            ]
+        );
+    }
 }
