ce3f62d7da694eeb78d9620d9c06e589215af267
Mirco Baalmans <mircobaalmans@live.nl>
2025-10-18 22:12:14 +0200

    Added trailing / to routes to keep code consistent

--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,16 @@ Route::get('/', function () {
 Route::middleware(['auth', 'verified'])->prefix('dashboard/')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-    Route::get('accounts', [AccountController::class, 'index'])->name('accounts');
+    Route::get('accounts/', [AccountController::class, 'index'])->name('accounts');
     Route::get('accounts/{account}', [AccountController::class, 'show'])->name('accounts.show');
-    Route::put('accounts', [AccountController::class, 'store'])->name('accounts.store');
+    Route::put('accounts/', [AccountController::class, 'store'])->name('accounts.store');
     Route::patch('accounts/{account}', [AccountController::class, 'update'])->name('accounts.update');
-    Route::delete('accounts', [AccountController::class, 'destroy'])->name('accounts.destroy');
+    Route::delete('accounts/', [AccountController::class, 'destroy'])->name('accounts.destroy');
 
-    Route::put('transactions', [TransactionController::class, 'store'])->name('transactions.store');
+    Route::put('transactions/', [TransactionController::class, 'store'])->name('transactions.store');
     Route::patch('transactions/{transaction}', [TransactionController::class, 'update'])->name('transactions.update');
     Route::delete('transactions', [TransactionController::class, 'destroy'])->name('transactions.destroy');
+    Route::delete('transactions/', [TransactionController::class, 'destroy'])->name('transactions.destroy');
 });
 
 require __DIR__.'/settings.php';
